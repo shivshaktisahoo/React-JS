a8580417501e2c45a39745f2c4d75e23a8bed931
shivshaktisahoo <shivsss9000@gmail.com>
2022-01-18 22:12:34 +0530

    class based

---
+++ my-app/src/App.js
@@ -0,0 +1,16 @@
+import React, {Component} from "react";
+
+
+class App extends Component{
+    render(){
+        return (
+            <div>
+                <h1>Hello My First React App</h1>
+                <h2>World</h2>
+            </div>
+        );
+    }
+}
+
+
+export default App;
