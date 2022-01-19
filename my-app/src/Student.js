d65b37cdd7f4527af32d5e2728d8066b40e21962
shivshaktisahoo <shivsss9000@gmail.com>
2022-01-19 21:56:24 +0530

    state

--- my-app/src/Student.js
+++ my-app/src/Student.js
@@ -1,14 +1,27 @@
-import React, { Component } from "react";
+import React, { Component } from 'react';
 
-// Functional Component
-// const Student = props =>{
-//     return <h1>Hello Shiv {props.children}</h1>
-// };
+// State used Without constructor
+// export default class Student extends Component{
+//         state = {
+//             name : "Shiv",
+//             roll : this.props.roll
+//         }   
+//     render(){
+//         return <h1>Hello, {this.state.name} & your roll no. is {this.state.roll}</h1>;
+//     }
+// }
 
-class Student extends Component {
+// State used Inside the constructor
+export default class Student extends Component{
+    constructor(props){
+        super(props);
+        this.state = {
+            name : "Shiv",
+            roll : this.props.roll
+        }
+    }
+    
     render(){
-        return <h1>Hello Shiv {this.props.children}</h1>;
+        return <h1>Hello, {this.state.name} & your roll no. is {this.state.roll}</h1>;
     }
 }
-
-export default Student;
--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,6 +1,5 @@
-import React from "react";
-import ReactDOM  from "react-dom";
-import Student from "./Student";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Student from './Student';
 
-// Rendering Component
-ReactDOM.render(<Student>I am Child {100+1}</Student>, document.getElementById("root"));
+ReactDOM.render(<Student roll="105"/>, document.getElementById("root"));
