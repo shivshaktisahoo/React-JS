15d4ce54e88852368a2901a367494a028034004c
shivshaktisahoo <shivsss9000@gmail.com>
2022-01-20 16:17:31 +0530

    argument passes to event handler

--- my-app/src/Student.js
+++ my-app/src/Student.js
@@ -3,37 +3,28 @@ import React, { Component } from 'react';
 
 // Class Component
 export default class Student extends Component{
-    constructor(props){
-        super(props)
-        this.state = {
-            name : "Shiv",
-            roll : this.props.roll,
-        }
+    // State w/o constructor or State Property
+    state = {
+        id : 1,
+        name : "Shiv",
+        roll : this.props.roll,
     }
-    // 1st Form
-    // handleClick = () => {
-    //    this.setState({name : "Ravi", roll : "105"});
-    // }
+    
+    // Event handler Arrow Function
+    handleClick = (id, e) => {
+       console.log("id", id);
+       console.log("event", e);
+    }
 
-    // 2nd form
-    handleClick = () => {
-       this.setState((state, props) => {
-           console.log(state);
-           console.log(state.name, state.roll);
-           console.log(props);
-           console.log(props.roll);
-           state.name = "Alok"
-           state.roll = "105"
-           return state
-       });
-    }
+
     render(){
         return(
             <div>
                 <h1>Hello, {this.state.name} & your Roll no. {this.state.roll}</h1>
-                <button onClick={this.handleClick}>Click Me</button>
+                {/* <button onClick={e => { this.handleClick(this.state.id, e);}}>Delete</button> */}
+                <button onClick={this.handleClick.bind(this, this.state.id)}>Delete</button>
             </div>
-        )
+        );
     }
 }
 
