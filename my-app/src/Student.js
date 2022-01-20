2f6d24e04b1db487068866ab790703df2584cca3
shivshaktisahoo <shivsss9000@gmail.com>
2022-01-20 15:40:38 +0530

    Update State - setState()

--- my-app/src/Student.js
+++ my-app/src/Student.js
@@ -1,40 +1,39 @@
-// import React, { Component } from 'react';
-import React from 'react';
+import React, { Component } from 'react';
 
 
 // Class Component
-// export default class Student extends Component{
-//     state = {
-//         name : "Shiv",
-//         roll : this.props.roll,
-//     }
-//     handleClick = () => {
-//         console.log("You clicked me !!!!", this);
-//     }
-//     render(){
-//         return(
-//             <div>
-//                 <h1>Hello Event {this.state.name} & {this.state.roll}</h1>
-//                 <button onClick={this.handleClick}>Click Me</button>
-//             </div>
-//         )
-//     }
-// }
+export default class Student extends Component{
+    constructor(props){
+        super(props)
+        this.state = {
+            name : "Shiv",
+            roll : this.props.roll,
+        }
+    }
+    // 1st Form
+    // handleClick = () => {
+    //    this.setState({name : "Ravi", roll : "105"});
+    // }
 
-// Function Component
-function Student(props) {
-    const handleClick = (e) => {
-        e.preventDefault();
-        console.log("You Clicked me !!!!");
-    };
-
-    return(
-        <div>
-            <h1>Hello Event {props.roll}</h1>
-            <a href="https://github.com/shivshaktisahoo" onClick={handleClick}>Click Me</a>
-            {/* <button onClick={handleClick}>Click me</button> */}
-        </div>
-    );
+    // 2nd form
+    handleClick = () => {
+       this.setState((state, props) => {
+           console.log(state);
+           console.log(state.name, state.roll);
+           console.log(props);
+           console.log(props.roll);
+           state.name = "Alok"
+           state.roll = "105"
+           return state
+       });
+    }
+    render(){
+        return(
+            <div>
+                <h1>Hello, {this.state.name} & your Roll no. {this.state.roll}</h1>
+                <button onClick={this.handleClick}>Click Me</button>
+            </div>
+        )
+    }
 }
 
-export default Student;
