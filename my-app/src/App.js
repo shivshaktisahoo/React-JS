082e31241b7f115d4bc37a500a4e096e45af54ad
shivshaktisahoo <shivsss9000@gmail.com>
2022-02-08 10:36:28 +0530

    CSS module

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,23 +1,13 @@
 import React, { Component } from 'react';
-import "./App.css";
 import User from './User';
+import styles from "./App.module.css";
 
 export default class App extends Component {
   render() {
-    let style = false;
-
-    // return (
-    //   <React.Fragment>
-    //     <h1 className="txt">Hello App</h1>
-    //     <User/>
-    //   </React.Fragment>
-    // );
-
-
     return (
       <React.Fragment>
-        <h1 className="txtr">Hello App</h1>
-        <User usercolor={style ? "txtr" : "txtg"}/>
+        <h1 className={styles.txt}>Hello App</h1>
+        <User />
       </React.Fragment>
     );
 
--- my-app/src/User.js
+++ my-app/src/User.js
@@ -1,11 +1,9 @@
 import React, { Component } from 'react';
-import "./User.css"
+import styles from "./User.module.css";
 
 export default class User extends Component {
   render() {
-    // return <h2 className='txt'>Hello Users</h2>;
+    return <h2 className={styles.txt}>Hello Users</h2>;
 
-
-    return <h2 className={this.props.usercolor}>Hello Users</h2>;
   }
 }
